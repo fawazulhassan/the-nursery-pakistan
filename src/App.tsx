f6d1d7e9e111924d56ca720159d5d2254ad305f9
fawazulhassan <fawazulhassan@gmail.com>
2026-04-14 16:53:04 +0500

    add analatic of website attach with vercel

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { CartProvider } from "@/context/CartContext";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
@@ -85,6 +86,7 @@ const App = () => (
           <BrowserRouter>
             <AppRoutes />
           </BrowserRouter>
+          <Analytics />
         </CartProvider>
       </AuthProvider>
     </TooltipProvider>
