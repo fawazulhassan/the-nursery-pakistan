a60303c7a9fe15c2fdd37ef7b7961a994fdc06c3
fawazulhassan <fawazulhassan@gmail.com>
2026-04-15 14:28:53 +0500

    adding speed insite of vercel

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { CartProvider } from "@/context/CartContext";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
@@ -87,6 +88,7 @@ const App = () => (
             <AppRoutes />
           </BrowserRouter>
           <Analytics />
+          <SpeedInsights />
         </CartProvider>
       </AuthProvider>
     </TooltipProvider>
